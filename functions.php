6623254c0cdeaad76d2749185303fbdd7c79bf9e
Nate <Nate@Easy-Tobacco-40.local>
2017-09-05 08:44:52 -0500

    adding in custom meta box to custom post types

--- functions.php
+++ functions.php
@@ -471,6 +471,62 @@ function create_post_type_shows()
     ));
 }
 
+// Define mix meta info
+
+function custom_mix_meta_box_markup($object)
+{
+  wp_nonce_field(basename(__FILE__), "meta-box-nonce");
+
+    ?>
+        <div>
+            <label for="meta-box-text">Soundcloud Link:</label>
+            <input name="meta-box-text" type="text" value="<?php echo get_post_meta($object->ID, "meta-box-text", true); ?>">
+
+        </div>
+    <?php
+
+}
+
+// Add mix meta info
+
+function add_mix_meta_box()
+{
+    add_meta_box("mix-meta-box", "Mix Info", "custom_mix_meta_box_markup", "mixes", "normal", "high", null);
+}
+
+add_action("add_meta_boxes", "add_mix_meta_box");
+
+
+// Save mix meta info
+
+function save_mix_meta_box($post_id, $post, $update)
+{
+    if (!isset($_POST["meta-box-nonce"]) || !wp_verify_nonce($_POST["meta-box-nonce"], basename(__FILE__)))
+        return $post_id;
+
+    if(!current_user_can("edit_post", $post_id))
+        return $post_id;
+
+    if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
+        return $post_id;
+
+    $slug = "post";
+    if($slug != $post->post_type)
+        return $post_id;
+
+    $meta_box_text_value = "";
+
+    if(isset($_POST["meta-box-text"]))
+    {
+        $meta_box_text_value = $_POST["meta-box-text"];
+    }
+    update_post_meta($post_id, "meta-box-text", $meta_box_text_value);
+}
+
+add_action("save_post", "save_mix_meta_box", 10, 2);
+
+
+
 /*------------------------------------*\
 	ShortCode Functions
 \*------------------------------------*/
